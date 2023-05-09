8fb9e1a896a8538feabe489bd440e6dc49be0c1c
Sergey Chaban <sergey.chaban@gmail.com>
2023-05-09 13:20:11 +0300

    use newer isl;

--- 1_get_arcs.sh
+++ 1_get_arcs.sh
@@ -29,7 +29,10 @@ wget $GNUFTP/binutils/binutils-2.37.tar.xz
 wget $GNUFTP/mpfr/mpfr-4.1.0.tar.xz
 wget $GNUFTP/gmp/gmp-6.2.1.tar.xz
 wget $GNUFTP/mpc/mpc-1.2.1.tar.gz
-wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2
+
+#wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2
+wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2
 wget https://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
+
 cd $_BASE_DIR_
 
